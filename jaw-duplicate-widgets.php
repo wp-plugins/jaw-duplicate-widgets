605574de79d2521d52bdbeec1c401f763abe6930
watson <watson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2006-10-25 21:40:31 +0000

    Added some useful comments, cleaned up some of the code and fixed a bug related to duplicating the text and rss widget.
    git-svn-id: https://plugins.svn.wordpress.org/jaw-duplicate-widgets/trunk@6746 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jaw-duplicate-widgets.php
+++ jaw-duplicate-widgets.php
@@ -3,7 +3,7 @@
 Plugin Name: JAW Duplicate Widgets
 Plugin URI: http://justaddwater.dk/wordpress-plugins/
 Description: Makes it possible to duplicate a widget so it can be used more than once (i.e. in two different sidebars or two times in the same sidebar). Requires the <a href="http://automattic.com/code/widgets/">Sidebar Widgets plugin</a> from Automattic.
-Version: 1.0.1
+Version: 1.0.2
 Author: Thomas Watson Steen
 Author URI: http://justaddwater.dk/
 */
@@ -13,32 +13,42 @@ class JAWDuplicateWidgets
 
 	function register()
 	{
+		// get a list of all the duplicate widgets
 		$options = get_option('jaw-duplicatewidget');
-		if(is_array($options))
+		if(is_array($options) && function_exists("register_sidebar_widget"))
 			foreach($options as $widget)
+				// for each widget traverse though its duplicates
 				foreach($widget as $duplicate)
-					if(is_array($duplicate) && function_exists("register_sidebar_widget"))
-						register_sidebar_widget($duplicate['name'], $duplicate['callback'], '', 'jaw-duplicatewidget', $duplicate);
+					if(is_array($duplicate))
+					{
+						array_push($duplicate['params'], "jaw_is_duplicate");
+						$params = array_merge(array($duplicate['name'], $duplicate['callback']), $duplicate['params']);
+						call_user_func_array("register_sidebar_widget", $params);
+					}
 	}
 
 	function setup()
 	{
 		global $registered_widgets;
 		$options = $newoptions = get_option('jaw-duplicatewidget');
-//		unset($newoptions);
+
 		if (isset($_POST['jaw-duplicatewidget-duplicate']))
 		{
 			$id = $_POST['jaw-duplicatewidget'];
 			
+			// if this widget isn't currently duplicated
 			if(empty($newoptions[$id]))
 				$newoptions[$id] = array();
 				
+			// run through each and every registered widget
 			foreach($registered_widgets as $name => $widget)
+				// this this is the widget we are trying to duplicate...
 				if($widget['id'] == $id)
 				{
+					// build up the new name (basically "oldname (copy X)")
 					$widget['name'] = $name . ' (' . __('copy') . ' ' . (count($newoptions[$id]) + 1) . ')';
-					$widget['parent_name'] = $name;
 					$dup_id = sanitize_title($widget['name']); // guess the new widget id
+					// add the duplicate widget to the list of duplicates
 					$newoptions[$id][$dup_id] = $widget;
 				}
 		}
@@ -61,27 +71,41 @@ class JAWDuplicateWidgets
 									unset($newoptions);
 							}
 		}
+
+		// if a widget has been duplicated or removed...
 		if ($options != $newoptions)
 		{
+			// save the changes...
 			$options = $newoptions;
 			update_option('jaw-duplicatewidget', $options);
 		}
+
+		// in the end: register all duplicate widgets (also newly added)
 		JAWDuplicateWidgets::register();
 	}
 	
 	function page() 
 	{
 		global $registered_widgets;
+		$options = get_option('jaw-duplicatewidget');
+
+		// build an index of the duplicated widgets
+		$dups = array();
+		if(is_array($options))
+			foreach($options as $duplicated_widget)
+				foreach($duplicated_widget as $dup_id => $duplicate)
+					$dups[$dup_id] = true;
 ?>
 		<div class="wrap">
 			<form method="post">
 				<h2>Duplicate Widgets</h2>
 				<p style="line-height: 30px;"><?php _e('Whitch widget would you like to duplicate?'); ?>
-				<select id="jaw-duplicatewidget" name="jaw-duplicatewidget" value="<?php echo $options['number']; ?>">
+				<select id="jaw-duplicatewidget" name="jaw-duplicatewidget">
 <?php
+		// check against the dup-index and output all widgets that are not duplicated
 		foreach ($registered_widgets as $name => $widget)
 		{
-			if($widget['params'][1] != 'jaw-duplicatewidget')
+			if(!$dups[$widget['id']])
 				print("<option value=\"{$widget['id']}\">" . __($name) . "</option>\n");
 		}
 ?>
@@ -91,7 +115,7 @@ class JAWDuplicateWidgets
 				</span>
 				</p>
 <?php
-		$options = get_option('jaw-duplicatewidget');
+		// if there are any duplicated widgets, list them
 		if(is_array($options))
 		{
 ?>
